cd04f17a53d7749ff6e812cb815eeb1cebc3541b
Artiom Ponomariov <artushock@gmail.com>
2021-07-09 11:56:22 +0300

    Added constraint layout for calculator

--- app/src/main/java/com/artushock/calculator/MainActivity.java
+++ app/src/main/java/com/artushock/calculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.artushock.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculator_layout);
+        setContentView(R.layout.calculator_constraint_layout);
+
+        log("onCreate");
+
+
+    }
+
+    private void log(String msg) {
+        Log.d("MAIN_ACTIVITY_1", msg);
     }
 }
